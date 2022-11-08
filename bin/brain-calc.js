12b9533b3d79ac4d50c74955f85f0da55ec211db
Svetlana177 <svetlanarozhkova177@mail.ru>
2022-11-08 12:38:27 +0500

    task 6 added brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { getRandomInRange, getMainParameters } from '../src/index.js';
+
+const minNumber = 1;
+const maxNumber = 10;
+const randomOperator = ['+', '-', '*'];
+const operators = {
+  '+': function plus(a, b) {
+    return a + b;
+  },
+  '-': function minus(a, b) {
+    return a - b;
+  },
+  '*': function multiplication(a, b) {
+    return a * b;
+  },
+};
+
+const questionText = 'What is the result of the expression?\nQuestion:';
+const calcParameters = () => {
+  const firstNumber = getRandomInRange(minNumber, maxNumber);
+  const secondNumber = getRandomInRange(minNumber, maxNumber);
+  const createRandom = getRandomInRange(0, randomOperator.length - 1);
+  const getRandomOperator = randomOperator[createRandom];
+
+  const question = `${questionText} ${firstNumber} ${getRandomOperator} ${secondNumber}`;
+  const result = operators[getRandomOperator](firstNumber, secondNumber);
+  return { question, result };
+};
+
+getMainParameters(calcParameters);
